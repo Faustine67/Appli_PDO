c7be418ccd207446436e613ba06961b3afa882c2
Faustine67 <michelfaustine@gmail.com>
2023-01-20 15:07:44 +0100

    ad css

--- index.php
+++ index.php
@@ -12,62 +12,63 @@
 
 <body>
 
-		<header>
-			<nav>
-				<h1> Brocoli </h1>
-				<div class="center">
-					<form>
-						<input type="search" id="search-input" placeholder="Rechercher...">
-						<button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-					</form>
-				</div>
-				<div class="right">
-					<a href="index.php">Ajout Article</a>
-					<a href="recap.php">
-						<i class="fa-solid fa-cart-shopping"></i>
-						<span>
-							<?php
-							if (isset($_SESSION['products'])) {
-								echo count($_SESSION['products']);
-							} else {
-								echo "0";
-							}
-							?>
-						</span>
-					</a>
-				</div>
-			</nav>
-		</header>
+	<header>
+		<nav>
+			<h1> Brocoli </h1>
+			<div class="center">
+				<form>
+					<input type="search" id="search-input" placeholder="Rechercher...">
+					<button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+				</form>
+			</div>
+			<div class="right">
+				<a href="index.php">Ajout Article</a>
+				<a href="recap.php">
+					<i class="fa-solid fa-cart-shopping"></i>
+					<span>
+						<?php
+						if (isset($_SESSION['products'])) {
+							echo count($_SESSION['products']);
+						} else {
+							echo "0";
+						}
+						?>
+					</span>
+				</a>
+			</div>
+		</nav>
+	</header>
 
-		<main>
-			<div class="big-image">
-				<h3> 100% genuine Products </h3>
+	<main>
+		<div class="big-image">
+			<div class="text">
+				<p> 100% genuine Products </p>
 				<h1> Tasty & Healthy Organic Food </h1>
-				<img src="Images/Bouquet-Legumes.jpg">
-				<button> Acheter Maintenant </button>
 			</div>
-			<div class="small-images">
-				<?php
-				require_once('db-functions.php');
-				$store = findAll();
+			<img src="Images/panier-legumes.png">
+		</div>
+		<div class="small-images">
+			<?php
+			require_once('db-functions.php');
+			$store = findAll();
 
-				foreach ($store as $product) { ?>
-					<form action="traitement.php?action=ajouterProduit&id=<?= $product["id"] ?>" method="post">
-						<section class="product-card" data-product-id="<?= $product["id"] ?>">
-							<a href="product.php?id=<?= $product["id"] ?>"><?= $product["nameProduct"] ?></a>
-							<p><?= $product["description"] ?></p>
-							<p><?= $product["price"] . "€" ?></p>
-							<p><input type="submit" name="submit" value="Ajouter le produit"></p>
-						</section>
-					</form>
+			foreach ($store as $product) { ?>
+				<form action="traitement.php?action=ajouterProduit&id=<?= $product["id"] ?>" method="post">
+					<section class="product-card" data-product-id="<?= $product["id"] ?>">
+						<a href="product.php?id=<?= $product["id"] ?>"><?= $product["nameProduct"] ?></a>
+						<p><?= $product["description"] ?></p>
+						<p><?= $product["price"] . "€" ?></p>
+						<p><input type="submit" name="submit" value="Ajouter le produit"></p>
+					</section>
+				</form>
 
-			</div>
-		</main>
-		</form>
+		</div>
+	</main>
+	</form>
 	</div>
 </body>
 <?php
-				}
+			}
 
 ?>
 
